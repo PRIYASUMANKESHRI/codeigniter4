71f17f32079cc503099104a3e8953d1e0d1ad00c
Priya <priyakeshri010@gmail.com>
2024-01-29 14:20:37 +0530

    Revert "name changes"
    This reverts commit bcde38fd376ede90ef32594dccc019982855136c.

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,7 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        $data['stuff']="Priya Suman Keshri";
+        $data['stuff']="Priya";
         return view('welcome_message',$data);
     }
 }
